a6f4b5c4e216a74700d81bc3aa05a1b64b4c7935
Gabe Fierro <gtfierro225@gmail.com>
2016-01-28 01:16:49 -0800

    Fix build scripts to program all attached motes

--- build_support.lua
+++ build_support.lua
@@ -2,7 +2,7 @@ local lfs = require "lfs"
 function go_build()
     if autoupdate == true then
         local cwd = lfs.currentdir()
-        lfs.chdir("toolchains/storm_elua")
+        lfs.chdir("toolchains/elua")
         os.execute("rm -f romfs/*")
         os.execute("git pull")
         lfs.chdir(cwd)
@@ -21,24 +21,32 @@ function go_build()
             pfx = pfx .. "KERNEL_WITH_ETH_SHIELD=TRUE"
         end
         print("executing make")
-        os.execute(string.format("%s make storm install", pfx))
+        if bulk_flash == true then
+            os.execute(string.format("%s make storm installall", pfx))
+        else
+            os.execute(string.format("%s make storm install", pfx))
+        end
         lfs.chdir(cwd)
     end
-    os.execute("rm -f toolchains/storm_elua/romfs/*")
+    os.execute("rm -f toolchains/elua/romfs/*")
     if autorun ~= nil then
         if lfs.attributes(autorun) == nil then
             print ("could not load autorun file")
             return
         end
-        lfs.link(string.format("%s/%s",lfs.currentdir(),autorun), "toolchains/storm_elua/romfs/autorun.lua", true)
+        lfs.link(string.format("%s/%s",lfs.currentdir(),autorun), "toolchains/elua/romfs/autorun.lua", true)
     end
     for k,v in pairs(libs) do
         if lfs.attributes(v) == nil then
             print (string.format("could not load library '%s'",k))
             return
         end
-        lfs.link(string.format("%s/%s",lfs.currentdir(),v),string.format("toolchains/storm_elua/romfs/%s.lua",k), true)
+        lfs.link(string.format("%s/%s",lfs.currentdir(),v),string.format("toolchains/elua/romfs/%s.lua",k), true)
     end
-    lfs.chdir("toolchains/storm_elua")
-    os.execute("./gobuild")
+    lfs.chdir("toolchains/elua")
+    if bulk_flash == true then
+        os.execute("./gobuildall")
+    else
+        os.execute("./gobuild")
+    end
 end
